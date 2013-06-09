e3b8437f900f7499846ce8e3f8d80c4f8b5bbf53
eternnoir <eternnoir@gmail.com>
2013-06-10 07:44:52 +0800

    add an Android httpPost example

---
+++ Android/HttpPost/src/com/example/htttppost/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.htttppost;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private Button _sendButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        _sendButton = (Button) findViewById(R.id.sendButton);
+        assert(_sendButton != null);
+        _sendButton.setOnClickListener(this);
+        return true;
+    }
+
+
+	@Override
+	public void onClick(View arg0) {
+		httpService _hs = new httpService();
+		EditText _urlText = (EditText) findViewById(R.id.urlText);
+		EditText _valueText = (EditText) findViewById(R.id.valueText);
+		String _url = _urlText.getText().toString();
+		String _value = _valueText.getText().toString();
+		TextView _result = (TextView) findViewById(R.id.resultTextView);
+		_result.setText(_hs.httpGet(_url, _value));	
+	}
+}
---
+++ Android/HttpPost/src/com/example/htttppost/httpService.java
@@ -0,0 +1,38 @@
+package com.example.htttppost;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class httpService {
+
+	public String httpGet(String url,String value){
+		String _ret = new String();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet get = new HttpGet(url+"?"+value);
+		try {
+			HttpResponse response = client.execute(get);
+			HttpEntity resEntity = response.getEntity();
+			_ret = EntityUtils.toString(resEntity);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "ERROR";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "ERROR";
+		}
+		catch (Exception e){
+			return"NOooooo \n check your url";
+		}
+		return _ret;
+	}
+}
